7ecf8968370bacf103b7166d04ef0d8a8a5a07a5
Keith Rarick <kr@xph.us>
2013-03-24 20:18:49 -0700

    readability

--- cto.go
+++ cto.go
@@ -3,8 +3,5 @@ package secureheaders
 // ContentTypeOptions returns a filter that sets header field
 // X-Content-Type-Options to "nosniff".
 func ContentTypeOptions() Filter {
-	return &HeaderFilter{
-		Field: "X-Content-Type-Options",
-		Value: "nosniff",
-	}
+	return &HeaderFilter{"X-Content-Type-Options", "nosniff"}
 }
