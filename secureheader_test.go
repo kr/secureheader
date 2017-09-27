2dbfb19f51812831d2c9c1cfdcb09a9fb7632515
Tom Maher <tmaher@users.noreply.github.com>
2017-09-26 18:07:59 -0700

    add CSP support (#13)
    Fixes #1.

--- secureheader_test.go
+++ secureheader_test.go
@@ -9,3 +9,33 @@ func TestDefaultUseForwardedProto(t *testing.T) {
 		t.Fatal("defaultUseForwardedProto = true want false")
 	}
 }
+
+func TestDefaultConfigHTTPSRedirect(t *testing.T) {
+	if !DefaultConfig.HTTPSRedirect {
+		t.Fatal("HTTPSRedirect = false want true")
+	}
+}
+
+func TestDefaultHSTSIncludeSubdomains(t *testing.T) {
+	if !DefaultConfig.HSTSIncludeSubdomains {
+		t.Fatal("HSTSIncludeSubdomains = false want true")
+	}
+}
+
+func TestDefaultHSTSPreload(t *testing.T) {
+	if DefaultConfig.HSTSPreload {
+		t.Fatal("HSTSPreload = true want false")
+	}
+}
+
+func TestDefaultCSP(t *testing.T) {
+	if DefaultConfig.CSP {
+		t.Fatal("CSP = true want false")
+	}
+}
+
+func TestDefaultCSPReportOnly(t *testing.T) {
+	if DefaultConfig.CSPReportOnly {
+		t.Fatal("CSPReportOnly = true want false")
+	}
+}
